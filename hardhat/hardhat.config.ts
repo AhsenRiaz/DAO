600a3d55c92702acdd3655440813e7914cd11956
ahsenrz <ahsenriaz2001@gmail.com>
2022-09-21 01:04:25 +0500

    hardhat

---
+++ hardhat/hardhat.config.ts
@@ -0,0 +1,12 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+  paths:{
+    artifacts:'../frontend/ABIs'
+  }
+
+};
+
+export default config;
