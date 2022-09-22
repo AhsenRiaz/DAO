7e77c342c3c49e628863105598d39f9fae06c202
ahsenrz <ahsenriaz2001@gmail.com>
2022-09-23 02:26:59 +0500

    Updated hardhat config

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,18 +1,26 @@
-import { HardhatUserConfig } from "hardhat/config";
-require('hardhat-docgen');
 import "@nomicfoundation/hardhat-toolbox";
+require("hardhat-docgen");
+require("dotenv").config();
 
-const config:any = {
+const config = {
   solidity: "0.8.17",
-  paths:{
-    artifacts:'../frontend/ABIs'
+  networks: {
+    rinkeby: {
+      url: process.env.RINKEBY_NETWORK_NODE,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.RINKEBY_ETHERSCAN_API_KEY,
+  },
+  paths: {
+    artifacts: "../frontend/ABIs",
   },
   docgen: {
-    path: './docs',
+    path: "./docs",
     clear: true,
     runOnCompile: true,
-  }
-  
+  },
 };
 
 export default config;
--- hardhat/scripts/deploy.ts
+++ hardhat/scripts/deploy.ts
@@ -1,23 +1,19 @@
+const hre = require("hardhat");
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const Dao = await ethers.getContractFactory("DAO");
+  // deploy the contract
+  const dao = await Dao.deploy("0x922b06cb1d709d570ab0fee31639a6d7322d01b2");
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  await dao.deployed();
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  console.log("Verify Contract Address:", dao.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
