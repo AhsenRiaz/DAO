ae9ede07bf2be7bcd422d0cb49d3fd2a1b5937a3
ahsenrz <ahsenriaz2001@gmail.com>
2022-09-22 23:58:28 +0500

    updated hardhat config

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,12 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
+require('hardhat-docgen');
 import "@nomicfoundation/hardhat-toolbox";
 
-const config: HardhatUserConfig = {
+const config:any = {
   solidity: "0.8.17",
   paths:{
     artifacts:'../frontend/ABIs'
+  },
+  docgen: {
+    path: './docs',
+    clear: true,
+    runOnCompile: true,
   }
-
+  
 };
 
 export default config;
