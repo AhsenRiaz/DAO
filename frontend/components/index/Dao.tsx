6e42887632455cc90b9c7bac16d28a5f725e5365
ahsenrz <ahsenriaz2001@gmail.com>
2022-09-23 23:14:27 +0500

    Update Dao.tsx

--- frontend/components/index/Dao.tsx
+++ frontend/components/index/Dao.tsx
@@ -1,18 +1,18 @@
 import { Card, Container, Grid, Text } from "@nextui-org/react";
 import Link from "next/link";
 import React, { FC } from "react";
-import { DaoList } from "./DaoList";
+import { DAO } from "../../types/ethers-contracts";
 
 interface IDao {
-  daoCards: DaoList;
+  daoProposals: DAO.ProposalStructOutput[];
 }
 
-const Dao: FC<IDao> = ({ daoCards }) => {
+const Dao: FC<IDao> = ({ daoProposals }) => {
   return (
     <Container lg>
       <Grid.Container gap={3}>
-        {daoCards &&
-          daoCards.map((dao, i) => {
+        {daoProposals &&
+          daoProposals.map((dao, i) => {
             return (
               <Link key={i} href={`dao/[id]`} as={`dao/${dao.id}`}>
                 <Grid key={i} xs={12} sm={6} md={4} lg={4}>
