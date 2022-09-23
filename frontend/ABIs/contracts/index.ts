d144f6fa1dfdc85f625ec21eeabe56562ce263fb
ahsenrz <ahsenriaz2001@gmail.com>
2022-09-23 23:15:34 +0500

    updated address for smart contract

--- frontend/ABIs/contracts/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as DAO_Contract } from "./DAO.sol/DAO.json";
-
-export const DAO_Contract_Address =
-  "0xA051278B3BfB884BB17425fDC70330Ae92D0FF02";
---
+++ frontend/ABIs/index.ts
@@ -0,0 +1,4 @@
+export { default as DAO_Contract } from "./contracts/DAO.sol/DAO.json";
+
+export const DAO_Contract_Address =
+  "0x4096B263f0986B30309D51BBe078dE52ce67e983";
