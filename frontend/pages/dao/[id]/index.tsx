7ea9aa4dba36b794a9c6c3a0e335e5f4011b8c31
ahsenrz <ahsenriaz2001@gmail.com>
2022-09-23 23:13:54 +0500

    vote dao page

--- frontend/pages/dao/[id]/index.tsx
+++ frontend/pages/dao/[id]/index.tsx
@@ -1,15 +1,140 @@
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import Link from "next/link";
-import { Container, Text } from "@nextui-org/react";
+import { Button, Container, Text } from "@nextui-org/react";
+import { useAppSelector } from "../../../redux/store";
+import { DAO } from "../../../types/ethers-contracts";
+import { useWeb3React } from "@web3-react/core";
+import Deadline from "../../../components/Deadline";
 
-const dao = ({}) => {
+const dao = () => {
+  const { daoContract } = useAppSelector((state) => state.loadContractsReducer);
+  const { account } = useWeb3React();
   const router = useRouter();
   const { id } = router.query;
+
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const [proposal, setProposal] = useState<DAO.ProposalStructOutput | null>(
+    null
+  );
+
+  const getDaoProposal = async () => {
+    try {
+      if (daoContract) {
+        let result = await daoContract.getProposal(id as string);
+        setProposal(result);
+        console.log("proposal", result);
+      }
+    } catch (error) {
+      console.log("Error in getting dao proposal", error);
+    }
+  };
+
+  const checkVotingEligibility = async (id: number, account: string) => {
+    try {
+      if (daoContract) {
+        let eligible = await daoContract.checkVotingEligibility(
+          id,
+          account.toLowerCase()
+        );
+        return eligible;
+      }
+    } catch (error) {}
+  };
+
+  const countVotes = async () => {
+    try {
+      if (daoContract && proposal) {
+        let id: string | number = proposal.id._hex;
+        id = parseInt(id);
+        let eligible = await daoContract.countVotes(id);
+      }
+    } catch (error) {}
+  };
+
+  const voteOnProposal = async (vote: boolean) => {
+    try {
+      if (daoContract && proposal) {
+        setLoading(true);
+        let id: string | number = proposal.id._hex;
+        id = parseInt(id);
+        // let eligible = await checkVotingEligibility(id, account as string);
+        // if (!eligible) {
+        //   throw "Not eligible";
+        // }
+        let transaction = await daoContract.voteOnProposal(id.toString(), vote);
+        let receipt = await transaction.wait();
+        if (receipt) {
+          console.log("receipt", receipt);
+          setLoading(false);
+        }
+      }
+    } catch (error) {
+      console.log("Error while voting proposal", error);
+    }
+  };
+
+  useEffect(() => {
+    getDaoProposal();
+  }, []);
+
+  useEffect(() => {}, [daoContract, account]);
 
   return (
     <div>
       <Container lg>
-        <Text h1>{id}</Text>
+        {proposal && (
+          <>
+            <Text css={{ color: "$headingFontColor" }} h2>
+              {proposal.title}
+            </Text>
+            <Text css={{ color: "$descriptionFontColor" }} h4>
+              Proposer {proposal.proposedBy}
+            </Text>
+            <Text css={{ color: "$descriptionFontColor" }} h4>
+              {proposal.description}
+            </Text>
+            <Text css={{ color: "$descriptionFontColor" }} h6>
+              Vote For Dao
+            </Text>
+            <Button onClick={() => voteOnProposal(true)} color={"gradient"}>
+              Vote For
+            </Button>
+            <Button
+              onClick={() => voteOnProposal(false)}
+              css={{ marginTop: "3rem" }}
+              color={"gradient"}
+            >
+              Vote Against
+            </Button>
+
+            <Text
+              css={{
+                marginTop: "2rem",
+                color: "$descriptionFontColor",
+                fontWeight: "bold",
+              }}
+            >
+              Can Vote
+            </Text>
+            {proposal.canVote.map((voter, i) => {
+              return (
+                <Text
+                  css={{
+                    color: "$descriptionFontColor",
+                    fontWeight: "$light",
+                  }}
+                >
+                  {voter}
+                </Text>
+              );
+            })}
+            <Deadline endTime={proposal.deadline} />
+            <Text>Vote For: {parseInt(proposal.voteUp._hex)}</Text>
+            <Text>Vote Against: {parseInt(proposal.voteDown._hex)}</Text>
+            <Button onClick={() => countVotes()}>Count Votes</Button>
+          </>
+        )}
       </Container>
     </div>
   );
